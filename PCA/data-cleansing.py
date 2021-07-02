ebd4211c4d036ce666b347e996d3f58e045b4fda
hyoseok <dddpower@gmail.com>
2021-07-02 16:41:31 +0900

    unique policy

--- PCA/data-cleansing.py
+++ PCA/data-cleansing.py
@@ -2,11 +2,11 @@ import pandas as pd
 from os import walk
 import sys
 
-temp = pd.read_excel('../resources/infection-result/서울.xlsx', nrows=0)
-temp_columns = temp.columns
-top_column = temp_columns.values.tolist()
-where = top_column[0]
-categories = top_column[3::4]
+# temp = pd.read_excel('../resources/infection-result/서울.xlsx', nrows=0)
+# temp_columns = temp.columns
+# top_column = temp_columns.values.tolist()
+# where = top_column[0]
+# categories = top_column[3::4]
 # print(where)
 # print(*categories, sep=", ")
 
@@ -25,7 +25,21 @@ filenames = []
 for (_, _, filename) in walk(path):
     filenames.extend(filename)
 
-print(*filenames)
-df = pd.read_csv(path + filenames[0], encoding='euc-kr')
-df['지역'] = filenames[0].split('.')[0]
-print(df)
+dfs = []
+for name in filenames:
+    df = pd.read_csv(path + name, encoding='euc-kr')
+    df['지역'] = name.split('.')[0]
+    dfs.append(df)
+    
+full_df = pd.concat(dfs)
+full_df = full_df.sort_values(by=['날짜'])
+
+seoul_df = full_df.loc[full_df['지역'] == '서울']
+test2 = seoul_df.drop_duplicates(subset=seoul_df.columns.values[1:])
+test = seoul_df.iloc[:, 1:]
+test2 = test2.sort_values(by=['단계'])
+test2.to_csv("drop_duplicate_seoul.csv")
+
+data = full_df.drop_duplicates(subset=full_df.columns.values[2:-1])
+data = data.sort_values(by=['단계'])
+data.to_csv("unique_policy.csv")
