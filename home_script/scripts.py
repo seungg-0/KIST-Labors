cb808f0e1f05e7f3031f806fdeb410896e150434
hyoseok <dddpower@gmail.com>
2021-07-06 17:14:49 +0900

    ing

--- home_script/scripts.py
+++ home_script/scripts.py
@@ -11,7 +11,7 @@ temp = paths.scoring
 file_list = listdir(temp)
 score_list = []
 for file_name in file_list:
-    df = pd.read_csv(temp + file_name, encoding='euc-kr', usecols=[0, 1], index_col=0)
+    df = pd.read_csv(temp + file_name, encoding='euc-kr', usecols=[0, 1])
     area_name = file_name.split(".")[0]
     df = df.iloc[:-7, :]
     score_list.append(df)
@@ -22,18 +22,18 @@ file_list = listdir(temp)
 df_list = []
 index = 0
 for file_name in file_list:
-    df = pd.read_csv(temp + file_name, encoding='euc-kr', index_col=0)
+    df = pd.read_csv(temp + file_name, encoding='euc-kr')
     area_name = file_name.split(".")[0]
     df["지역"] = area_name
-    df = df.rename(columns={"신고일": "확진자수"})
+    df = df.rename(columns={"신고일": "확진자수", "data": "날짜"})
     df["비율"] = df["확진자수"] / population[area_name]
     df["단계"] = score_list[index]["단계"]
     df.to_csv(area_name + "_비율.csv", index=False, encoding='euc-kr')
     df_list.append(df)
     index += 0
 
-print(df_list[1])
+full_df = pd.concat(df_list)
+print(full_df)
+full_df.to_csv("종합_비율.csv", index=False, encoding='euc-kr')
 
-
-print(score_list[1])
 
